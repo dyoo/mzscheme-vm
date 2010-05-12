e7546fbea2c5b19ba8353ae71d89d0c5246c1454
dyoo <dyoo@kfisler-ra1.(none)>
2010-05-12 10:43:47 -0400

    Added silly test for branch.

--- t/tests.js
+++ t/tests.js
@@ -2,25 +2,22 @@ var assert = require('assert');
 var runtime = require('./../lib');
 var sys = require('sys');
 
+var makeConstant = function(c) { return {$:'constant', value:c}; };
 
 
 
-
-//////////////////////////////////////////////////////////////////////
+// Simple running should just terminate.
 (function() { 
-
     var state = new runtime.State();
     state.run();
 })();
 
 
 
-//////////////////////////////////////////////////////////////////////
-
-// Constants
+// Numeric constant
 (function() {
     var state = new runtime.State();
-    state.pushControl({$:'constant', value: 42});
+    state.pushControl(makeConstant(42));
     var result = state.run();
     assert.deepEqual(result, 
 		     42);
@@ -28,11 +25,39 @@ var sys = require('sys');
 
 
 
+// String constant.
 (function() {
     var state = new runtime.State();
-    state.pushControl({$:'constant', value:"hello world"});
+    state.pushControl(makeConstant("hello world"));
     var result = state.run();
     assert.deepEqual(result, 
 		     "hello world");
 })();
-//////////////////////////////////////////////////////////////////////
+
+
+
+
+// Simple branch to true
+(function() {
+    var state = new runtime.State();
+    state.pushControl({'$' : 'branch', 
+		       'test' : makeConstant(true),
+		       'then' : makeConstant(true),
+		       'else' : makeConstant(false)});
+    var result = state.run();
+    assert.deepEqual(result, 
+		     true);
+})();
+
+
+// Simple branch to false
+(function() {
+    var state = new runtime.State();
+    state.pushControl({'$' : 'branch', 
+		       'test' : makeConstant(false),
+		       'then' : makeConstant(false),
+		       'else' : makeConstant(true)});
+    var result = state.run();
+    assert.deepEqual(result, 
+		     true);
+})();
