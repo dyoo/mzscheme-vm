3008ccb06e8fbaf476d7e1fbb7b6f941b2b61d6a
Ethan Benjamin Cecchetti <ececchet@cs.brown.edu>
2010-06-22 14:46:04 -0400

    fixed structure inheritence and changed error throwing slightly

--- tests/unit-tests/imports.js
+++
@@ -1,9 +0,0 @@
-// Imports for testing with node
-
-var assert = require('assert');
-// var runtime = require('./../../lib/test-support');
-//var sys = require('sys');
-
-//var control = runtime.control;
-//var types = runtime.types;
-
