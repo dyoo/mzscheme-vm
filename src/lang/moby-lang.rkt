81b65d2966bf583d2535849dd913e4861e0d0a81
Danny Yoo <dyoo@cs.wpi.edu>
2010-09-07 15:04:49 -0400

    right in the middle of implementing with-handlers.

--- src/lang/moby-lang.rkt
+++ src/lang/moby-lang.rkt
@@ -1,6 +1,29 @@
 #lang s-exp "kernel.rkt"
 
-(require "kernel.rkt")
-(require "paramz.rkt")
+(require "kernel.rkt"
+         "paramz.rkt"
+         (for-syntax racket/base))
 
 (provide (all-from-out "kernel.rkt"))
+(provide with-handlers)
+
+
+(define-syntax (with-handlers stx)
+  (syntax-case stx ()
+    [(_ ([test? exn-handler] ...) body ...)
+     (syntax/loc stx
+       (let ([prompt-tag (make-continuation-prompt-tag)])
+         (with-continuation-mark exception-handler-key
+           (lambda (exn)
+             (abort-current-continuation prompt-tag exn))
+           (call-with-continuation-prompt 
+            (lambda ()
+              (begin body ...))
+            prompt-tag
+            (lambda (exn)
+              (cond
+                [(test? exn)
+                 (exn-handler exn)]
+                ...
+                [else
+                 (raise exn)]))))))]))
