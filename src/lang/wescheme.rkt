23c102330be9841ac3941e691b2e69d802390768
Danny Yoo <dyoo@cs.wpi.edu>
2010-09-13 14:28:51 -0400

    wescheme stuff

---
+++ src/lang/wescheme.rkt
@@ -0,0 +1,28 @@
+#lang s-exp "moby-lang.rkt"
+
+;; This is the default language module for WeScheme programs.
+;;
+;; It implements a "pretty-big"-like language that'll expose everything
+;; in one big module, just so users don't have to worry about
+;; doing things with require.
+
+
+(provide (except-out (all-from-out "moby-lang.rkt")
+                     
+                     ;; We provide everything from moby-lang except
+                     ;; syntax stuff, because it's not quite safe
+                     ;; to do arbitrary server-side compilation.
+                     define-syntax
+                     define-for-syntax
+                     begin-for-syntax
+                     for-syntax))
+
+
+
+
+
+;; For compatibility, re-exports image-url as open-image-url.
+(define open-image-url image-url)
+(provide open-image-url)
+
+
