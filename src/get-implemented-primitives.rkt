e5bc148d1cec60d26cf1556a82b2bcb755cdac57
Danny Yoo <dyoo@cs.wpi.edu>
2010-09-16 16:20:01 -0400

    added module to get at the list of implemented primitives.

--- src/get-implemented-primitives.rkt
+++ src/get-implemented-primitives.rkt
@@ -1,7 +1,8 @@
 #lang racket/base
 
 (require racket/runtime-path
-         racket/file)
+         racket/file
+         racket/contract)
 ;; Get the list of primitives implemented in primitives.js
 
 (define-runtime-path primitive.js "../lib/primitive.js")
@@ -15,12 +16,15 @@
             name)
           string<?)))
         
-(define names
-  (sort&unique
-   (map (lambda (a-str)
-          (substring a-str
-                     (string-length "PRIMITIVES['")
-                     (- (string-length a-str) (string-length "']"))))
-        (let ([contents (file->string primitive.js)])
-          (regexp-match* #px"PRIMITIVES\\[('|\")[^\\]]*('|\")\\]" contents)))))
+;; primitive-names: (listof symbol)
+(define primitive-names
+  (map string->symbol
+       (sort&unique
+        (map (lambda (a-str)
+               (substring a-str
+                          (string-length "PRIMITIVES['")
+                          (- (string-length a-str) (string-length "']"))))
+             (let ([contents (file->string primitive.js)])
+               (regexp-match* #px"PRIMITIVES\\[('|\")[^\\]]*('|\")\\]" contents))))))
      
+(provide/contract [primitive-names (listof symbol?)])
