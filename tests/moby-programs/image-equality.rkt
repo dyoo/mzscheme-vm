d842277238e265a7e57dfe792eff2682b4dc77a3
Danny Yoo <dyoo@cs.wpi.edu>
2010-09-29 12:42:33 -0400

    more tests on image equality and construction.

--- tests/moby-programs/image-equality.rkt
+++ tests/moby-programs/image-equality.rkt
@@ -16,23 +16,26 @@
 
 ;; Let's show these at the toplevel to make sure the drawing is working
 ;; ok
-a-circle
-a-star
-a-nw-rect
-a-rect
-a-triangle
-a-line
-a-text
+(printf "should be a circle:") a-circle
+(printf "should be a star:") a-star
+(printf "should be a nw:rectangle:") a-nw-rect
+(printf "should be a rectangle:") a-rect
+(printf "should be a triangle:") a-triangle
+(printf "should be a line:") a-line
+(printf "should be a text:") a-text
 
 
 ;; check-fail-contract: (-> void) -> void
 ;; Make sure we fail with a contract error.
 (define (check-fail-contract thunk)
   (with-handlers ([exn:fail:contract? void])
-    (thunk)))
+    (thunk)
+    (raise (format "failure expected: ~s" thunk))))
+
 
 
 ;; Now do image comparisons
+(printf "running image comparison tests\n")
 
 ;; circles
 (check-expect (equal? (circle 20 'solid 'green)
@@ -45,7 +48,9 @@ a-text
                       (circle 20 'solid 'blue))
               false)
 (check-fail-contract (lambda () (circle 20 "foobar" "green")))
+(check-fail-contract (lambda () (circle 20 "outline" "not-a-color")))
 (check-fail-contract (lambda () (circle 20 'green 'outline)))
+(check-fail-contract (lambda () (circle 'green 'outline 20)))
 
 
 
@@ -66,7 +71,9 @@ a-text
                       (star 20 10 60 'solid 'magenta))
               false)
 (check-fail-contract (lambda () (star 20 10 60 "foobar" 'green)))
+(check-fail-contract (lambda () (star 20 10 60 "outline" 'not-a-color)))
 (check-fail-contract (lambda () (star 20 10 60 "green" 'outline)))
+(check-fail-contract (lambda () (star 10 60 "green" 'outline 20)))
  
 
 ;; nw:rect
@@ -86,7 +93,9 @@ a-text
                       (nw:rectangle 10 20 'outline 'white))
               false)
 (check-fail-contract (lambda () (nw:rectangle 10 20 "foobar" 'green)))
+(check-fail-contract (lambda () (nw:rectangle 10 20 "outline" 'not-a-color)))
 (check-fail-contract (lambda () (nw:rectangle 10 20 'green 'outline)))
+(check-fail-contract (lambda () (nw:rectangle 20 'green 'outline 10)))
 
 
 ;; rect
@@ -106,11 +115,55 @@ a-text
                       (rectangle 10 20 'outline 'white))
               false)
 (check-fail-contract (lambda () (rectangle 10 20 "foobar" 'green)))
+(check-fail-contract (lambda () (rectangle 10 20 "outline" 'not-a-color)))
 (check-fail-contract (lambda () (rectangle 10 20 'green 'outline)))
+(check-fail-contract (lambda () (rectangle 20 'green 'outline 10)))
 
 
 ;; triangle
+(check-expect (equal? (triangle 10 'solid 'green)
+                      (triangle 10 'solid 'green))
+              true)
+
+(check-expect (equal? (triangle 10 'solid 'green)
+                      (triangle 9 'solid 'green))
+              false)
+(check-expect (equal? (triangle 10 'solid 'green)
+                      (triangle 10 'outline 'green))
+              false)
+(check-expect (equal? (triangle 10 'solid 'green)
+                      (triangle 10 'solid 'olive))
+              false)
+(check-fail-contract (lambda () (triangle 10 'foobar 'green)))
+(check-fail-contract (lambda () (triangle 10 'outline 'not-a-color)))
+(check-fail-contract (lambda () (triangle 10 'green 'outline)))
+(check-fail-contract (lambda () (triangle 'green 'outline 10)))
+
 
 ;; line
+(check-expect (equal? (line 10 20 'blue)
+                      (line 10 20 'blue))
+              true)
+(check-expect (equal? (line 10 20 'blue)
+                      (line 20 10 'blue))
+              false)
+(check-fail-contract (lambda () (line 10 20 'not-a-color)))
+(check-fail-contract (lambda () (line 'not-a-color 20 10)))
+
 
 ;; text
+(check-expect (equal? (text "hello" 20 'yellow)
+                      (text "hello" 20 'yellow))
+              true)
+
+(check-expect (equal? (text "hello" 20 'yellow)
+                      (text "hi" 20 'yellow))
+              false)
+(check-fail-contract (lambda () (text 'hi 20 'yellow)))
+(check-fail-contract (lambda () (text "hello" 'yellow 20)))
+
+
+              
+
+
+(printf "ran image comparison tests\n")
