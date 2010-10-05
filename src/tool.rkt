784915772010a23676daec02749422538694c540
dyoo <dyoo@kfisler-ra1.(none)>
2010-10-05 13:47:45 -0400

    about to add the menu item.

--- src/tool.rkt
+++ src/tool.rkt
@@ -2,9 +2,15 @@
 
 (require #;racket/gui/base
          racket/unit
+         racket/class
          drracket/tool)
 
+;; This tool adds a "Create Javascript Package" button to the Racket menu.
+
+
 (provide tool@)
+
+
 
 (define tool@
   (unit
@@ -15,4 +21,20 @@
       (void))
     
     (define (phase2)
-      (void))))
+      (void))
+
+    (define unit-frame<%> (class->interface drracket:unit:frame%))
+
+    ;; Here we mix in a button.
+    (drracket:get/extend:extend-unit-frame
+     (mixin (unit-frame<%>) (unit-frame<%>)
+       (inherit get-language-menu)
+       
+       (super-new)
+       
+       (printf "~s\n" (get-language-menu))
+       
+       
+       ))
+    
+    (printf "loading mzscheme-vm tool\n")))
