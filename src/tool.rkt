ae06e033b088e6a52707517f9f71f6e7aea16b78
dyoo <dyoo@kfisler-ra1.(none)>
2010-10-05 14:14:03 -0400

    added menu button; now need to implement the functionality.

--- src/tool.rkt
+++ src/tool.rkt
@@ -1,8 +1,9 @@
 #lang racket/base
 
-(require #;racket/gui/base
+(require racket/gui/base
          racket/unit
          racket/class
+         racket/path
          drracket/tool)
 
 ;; This tool adds a "Create Javascript Package" button to the Racket menu.
@@ -17,24 +18,48 @@
     (import drracket:tool^)
     (export drracket:tool-exports^)
     
-    (define (phase1)
-      (void))
-    
-    (define (phase2)
-      (void))
+    ;; We're not doing anything language specific, so I don't think we need
+    ;; to plug into phase1 or phase2.
+    (define (phase1) (void))
+    (define (phase2) (void))
 
+
+    
+    ;; unit-frame<%>: interface
+    ;; Just a helper interface used for the mixin below.
     (define unit-frame<%> (class->interface drracket:unit:frame%))
 
-    ;; Here we mix in a button.
+    ;; Here we mix in a menu item into the unit frame's Racket menu.
     (drracket:get/extend:extend-unit-frame
      (mixin (unit-frame<%>) (unit-frame<%>)
-       (inherit get-language-menu)
+       (inherit get-language-menu
+                get-definitions-text)
+    
+       
+       ;; create-javascript-package: menu-item% control-event% -> void
+       (define (create-javascript-package a-menu-item a-control-event)
+         (let* ([a-text (get-definitions-text)]
+                [a-filename (send a-text get-filename)])
+           (cond
+             [(not (path-string? a-filename))
+              (message-box "Create Javascript Package"
+                           "Your program needs to be saved first before packaging.")
+              ]
+             [(send a-text is-modified?)
+              (message-box "Create Javascript Package"
+                           "Your program has changed since your last save or load; please save before packaging.")]
+             [else
+              (printf "Asked to generate package for ~s\n"
+                      (normalize-path a-filename))
+              ])))
+                  
+       
        
        (super-new)
        
-       (printf "~s\n" (get-language-menu))
-       
-       
-       ))
-    
-    (printf "loading mzscheme-vm tool\n")))
+       (let ([racket-menu (get-language-menu)])
+         (new separator-menu-item% [parent racket-menu])
+         (new menu-item% 
+              [parent racket-menu]
+              [label "Create Javascript Package"]
+              [callback create-javascript-package]))))))
