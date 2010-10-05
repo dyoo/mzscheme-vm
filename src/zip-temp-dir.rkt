7bd6fb13b0b60e1df4b62f6ac91f625939cee602
dyoo <dyoo@kfisler-ra1.(none)>
2010-10-05 14:42:38 -0400

    helper for making it easier to use scratch directories and get zip archives of the result.

---
+++ src/zip-temp-dir.rkt
@@ -0,0 +1,30 @@
+#lang racket/base
+
+(require file/zip
+         racket/file)
+
+(zip-verbose #t)
+
+
+;; call-with-temporary-directory->zip: string (path -> X) -> (values output-port X)
+(define (call-with-temporary-directory->zip dirname with-path-f)  
+  (let* ([tempdir
+          (make-temporary-file "mztmp~a" 'directory #f)]
+         [workdir
+          (build-path tempdir dirname)])
+    
+    (dynamic-wind
+     
+     (lambda ()
+       (make-directory workdir))
+     
+     (lambda ()
+       (let ([result (with-path-f workdir)])
+         (let-values ([(inp outp) (make-pipe)])
+           (parameterize ([current-directory tempdir])
+             (zip->output (list workdir) outp)
+             (close-output-port outp)
+             (values inp result)))))
+     
+     (lambda ()
+       (delete-directory/files tempdir)))))
