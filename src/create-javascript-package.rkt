d0ec253e443e9a2dfba0d57b9b5a5e9c6bdbd0d5
dyoo <dyoo@kfisler-ra1.(none)>
2010-10-12 12:17:18 -0400

    Some refactoring, in preparation for moby phone packaging.

--- src/create-javascript-package.rkt
+++ src/create-javascript-package.rkt
@@ -32,12 +32,9 @@
     (call-with-output-file (build-path output-directory "program.js")
       (lambda (op)
         ;; First, write out all the collections.
-        (for ([r module-records])
-          (fprintf op "MODULES[~s] = ~a;\n\n"
-                   (symbol->string (module-record-name r))
-                   #;(path->string (find-relative-path base-dir (module-record-path r)))
-                   (encode-module-record r base-dir)))
-        ;; Designate one of the collections:
+        (write-module-records module-records op)
+        
+        ;; Designate one of the collections as the main one.
         (for ([r module-records])
           (cond
             [(string=? (path->string (module-record-path r))
@@ -50,8 +47,21 @@
       #:exists 'replace)))
 
 
+;; write-module-records: (listof module-record) path output-port -> void
+;; Writes out the module records.  Each module record will be written out as
+;; an assignment into the MODULES object by default.
+;;    MODULES[module-name] = ...
+(define (write-module-records module-records op #:assign-into (assign-into "MODULES"))
+  (for ([r module-records])
+    (fprintf op "~a[~s] = ~a;\n\n"
+             assign-into
+             (symbol->string (module-record-name r))
+             #;(path->string (find-relative-path base-dir (module-record-path r)))
+             (encode-module-record r))))
+
+
 ;; encode-module-record: module-record path -> string
-(define (encode-module-record r base-path)
+(define (encode-module-record r)
   (when (not (empty? (module-record-unimplemented-primval-references r)))
     (log-warning 
      (format "WARNING: while compiling ~a, we ran across the following problem:\nThe following primitives are not yet implemented in the Javascript runtime:\n~a\nThe resulting Javascript will likely not evaluate.\n"
@@ -77,4 +87,6 @@
 
 
 (provide/contract [create-javascript-package
-                   (path? path? . -> . any)])
+                   (path? path? . -> . any)]
+                  [write-module-records
+                   (((listof module-record?) output-port?) (#:assign-into string?) . ->* . any)])
