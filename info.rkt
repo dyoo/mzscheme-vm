d688498a9d5e6a844d683a0bcbfcbd0f133cd3fe
Danny Yoo <dyoo@cs.wpi.edu>
2010-10-14 14:29:18 -0400

    adding PLaneT information in the info.rkt.

--- info.rkt
+++ info.rkt
@@ -7,3 +7,12 @@
 
 (define scribblings '(("doc/jsworld.rkt")
 		      ("doc/image.rkt")))
+
+
+(define categories '(devtools))
+(define can-be-loaded-with 'all)
+(define required-core-version "5.0.1")
+(define version "0.1alpha")
+(define repositories "4.x")
+(define blurb 
+  '("Provides support for compiling Racket programs to run as Javascript on the browser."))
