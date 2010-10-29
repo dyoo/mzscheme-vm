6e65b7e781ba8506c997df59d6384cf6484589e5
Danny Yoo <dyoo@cs.wpi.edu>
2010-10-29 19:15:19 -0400

    trying to make test case for ffi

---
+++ tests/moby-programs/ffi.rkt
@@ -0,0 +1,14 @@
+#lang s-exp "../../lang/base.rkt"
+
+
+(require "../../ffi/ffi.rkt")
+
+
+(define window
+  (js-get-global-value "window"))
+
+(define platform
+  (js-get-field window "navigator" "platform"))
+
+(printf "Current browser platform is ~s\n" platform)
+
