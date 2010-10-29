7840ec35a67e666ab2511efb2486e63dd9d66429
Danny Yoo <dyoo@cs.wpi.edu>
2010-10-29 19:30:14 -0400

    continuing to test the ffi library, since it's going to be critical that it works well

--- tests/moby-programs/ffi.rkt
+++ tests/moby-programs/ffi.rkt
@@ -17,9 +17,35 @@
 
 (check-expect (prim-js->scheme (scheme->prim-js "hello world"))
 	      "hello world")
-(check-expect (prim-js->scheme (scheme->prim-js 42))
-	      42)
 (check-expect (prim-js->scheme (scheme->prim-js #t))
 	      #t)
 (check-expect (prim-js->scheme (scheme->prim-js #f))
 	      #f)
+
+
+;; (prim-js->scheme (scheme->prim-js ...)) is not the identity, unfortunately.
+;; Here are tests that show that we need to do something:
+
+;; Numbers that come back to us are inexact.
+(check-expect (inexact->exact (prim-js->scheme (scheme->prim-js 42)))
+	      42)
+
+;; Characters are mapped to one-character strings.
+(check-expect (prim-js->scheme (scheme->prim-js #\h))
+	      "h")
+
+;; Symbols are mapped to strings.
+(check-expect (prim-js->scheme (scheme->prim-js 'hello))
+	      "hello")
+
+;; Note that when converting vectors, the resulting inner values are not
+;; automatically transformed back.  So the prim-js->scheme transformation 
+;; is shallow.
+(check-expect (map prim-js->scheme 
+		   (vector->list (prim-js->scheme (scheme->prim-js #(1 2 3)))))
+	      '(1.0 2.0 3.0))
+
+(check-expect (map prim-js->scheme 
+		   (vector->list (prim-js->scheme 
+				  (scheme->prim-js #(1 "testing" 3)))))
+	      '(1.0 "testing" 3.0))
