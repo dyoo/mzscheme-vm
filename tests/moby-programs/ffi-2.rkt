1c0f2db5f3d36bc4c6b86806129e18e83411e72b
Danny Yoo <dyoo@cs.wpi.edu>
2010-10-29 21:59:44 -0400

    added a my-on-tick just to make sure world-config is doing something reasonable.

---
+++ tests/moby-programs/ffi-2.rkt
@@ -0,0 +1,40 @@
+#lang s-exp "../../lang/base.rkt"
+(require "../../ffi/ffi.rkt"
+	 "../../jsworld/jsworld.rkt"
+	 "../../lang/check-expect/check-expect.rkt")
+
+"my-ffi-2.rkt"
+
+;; Check to see that we can expression on-tick with make-world-config.
+
+
+(define (my-on-tick world-updater)
+  (make-world-config
+   (lambda (success)
+     (js-call (js-get-global-value "setInterval")
+	      #f
+	      success
+	      1000))
+
+   (lambda (id)
+     (printf "shutdown with clearInterval id=~s\n" id)
+     (js-call (js-get-global-value "clearInterval")
+	      #f
+	      id))
+
+   (lambda (w)
+     (world-updater w))))
+
+
+
+
+(check-expect (big-bang 1
+			(my-on-tick 
+			 (lambda (w)
+			   (printf "tick!\n")
+			   (add1 w)))
+			(stop-when 
+			 (lambda (n) (= n 10))))
+	      10)
+
+"end my-ffi-2.rkt"
