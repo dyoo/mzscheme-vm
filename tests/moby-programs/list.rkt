126295cf346afe1b40f822b262e6db8f15abb172
Danny Yoo <dyoo@cs.wpi.edu>
2010-11-03 15:38:24 -0400

    changing list* so it takes arbitrary arguments.

--- tests/moby-programs/list.rkt
+++ tests/moby-programs/list.rkt
@@ -3,10 +3,13 @@
 
 "list tests"
 
+(check-expect (list* 4)
+	      4)
+
 (check-expect (list* 1 2 3)
 	      (cons 1 (cons 2 3)))
 
-(check-expect (list* 1 2 (3))
+(check-expect (list* 1 2 '(3))
 	      (cons 1 (cons 2 (cons 3 empty))))
 
 "end list tests"
