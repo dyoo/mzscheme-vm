980bae936eaf512baa20ce9ab37087c012585cbe
Danny Yoo <dyoo@cs.wpi.edu>
2010-11-03 13:06:34 -0400

    adding another small test to cycles

--- tests/moby-programs/cycles.rkt
+++ tests/moby-programs/cycles.rkt
@@ -19,3 +19,22 @@
 (shared ([a (box a)])
   (check-expect (format "~s\n" a)
                 "#&...\n"))
+
+
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define my-list (shared ([x (cons 1 (cons 2 (cons 3 y)))]
+                         [y (cons 'a (cons 'b (cons 'c  (cons 'd x))))])
+                  x))
+(define (take n l)
+  (cond
+    [(= n 0)
+     empty]
+    [else
+     (cons (first l)
+           (take (sub1 n) (rest l)))]))
+
+(check-expect (take 20 my-list)
+	      '(1 2 3 a b c d 1 2 3 a b c d 1 2 3 a b c))
