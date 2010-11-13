94f98611594d17bca37fe3fffa960a2c9b2bf37b
Danny Yoo <dyoo@cs.wpi.edu>
2010-11-12 22:11:00 -0500

    added test for begin0

--- tests/moby-programs/begin.rkt
+++ tests/moby-programs/begin.rkt
@@ -1,7 +1,25 @@
-#lang s-exp "../../lang/base.ss"
+#lang s-exp "../../lang/wescheme.ss"
 
 (printf "begin.rkt\n")
 (printf "You should see the string \"hello world\" immediately after this: ")
 
 (begin (printf "hello ")
        (printf "world\n"))
+
+
+
+(let ([counter 17])
+  (check-expect (begin
+		  counter
+		  (set! counter (add1 counter))
+		  counter)
+		18))
+
+
+(let ([counter 17])
+  (check-expect (begin0 counter
+			(set! counter (add1 counter))
+			counter)
+		17))
+
+"begin.rkt end"
