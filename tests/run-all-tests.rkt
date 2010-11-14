073bbac3071b07105936e55e7a2a5abb8a0e7ae9
Danny Yoo <dyoo@cs.wpi.edu>
2010-11-14 02:12:32 -0500

    adjusting the test procedure to yell if we have wescheme primitives that have not even been touched by the moby-programs test suite

--- tests/run-all-tests.rkt
+++ tests/run-all-tests.rkt
@@ -1,3 +1,11 @@
 #lang racket/base
-(require "../main.rkt")
+(require racket/list
+	 "../main.rkt"
+	 "check-coverage.rkt")
+
+(unless (empty? (untouched-wescheme-primitives))
+  (print-coverage-report)
+  (printf "Press Enter to continue.\n")
+  (void (read-line)))
+
 (run-in-browser "all-tests.rkt")
