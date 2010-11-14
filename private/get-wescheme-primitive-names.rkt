db98865f1ce650fc9fe835aeb761092cccbbad12
Danny Yoo <dyoo@cs.wpi.edu>
2010-11-14 01:47:23 -0500

    adding small lookup table of all the wescheme names

---
+++ private/get-wescheme-primitive-names.rkt
@@ -0,0 +1,13 @@
+#lang racket/base
+(require racket/runtime-path)
+
+(define-runtime-path wescheme-module-path "../lang/wescheme.rkt")
+  
+(define wescheme-primitive-names
+  (let ([ns (make-base-empty-namespace)])
+    (parameterize ([current-namespace ns])
+      (namespace-require `(file ,(path->string wescheme-module-path)))
+      (namespace-mapped-symbols))))
+
+(provide wescheme-primitive-names)
+
