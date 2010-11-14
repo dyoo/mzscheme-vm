fc6058a977ce54ca76c9cd49c2b1fb5acfb7c4ea
Danny Yoo <dyoo@cs.wpi.edu>
2010-11-14 02:25:36 -0500

    added the call to the internal VM tests

--- tests/run-all-tests.rkt
+++ tests/run-all-tests.rkt
@@ -1,11 +1,24 @@
 #lang racket/base
 (require racket/list
 	 "../main.rkt"
+	 racket/runtime-path
+	 racket/system
 	 "check-coverage.rkt")
 
+(define-runtime-path my-directory ".")
+
+(printf "Running the VM internal test suite\n")
+(parameterize ([current-directory my-directory])
+  (system "unit-tests/run-tests")
+  (printf "Press Enter to continue.\n")
+  (void (read-line)))
+
+
+(printf "Checking for untouched wescheme primitives\n")
 (unless (empty? (untouched-wescheme-primitives))
   (print-coverage-report)
   (printf "Press Enter to continue.\n")
   (void (read-line)))
 
+(printf "Running browser tests\n")
 (run-in-browser "all-tests.rkt")
