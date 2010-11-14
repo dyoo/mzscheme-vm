6cbd10f445a73aaf7ff69758c83ed109a87c0ae4
Danny Yoo <dyoo@cs.wpi.edu>
2010-11-13 22:52:41 -0500

    predicates for exn:fail appear not to be distinguishing correctly.

--- tests/moby-programs/raise.rkt
+++ tests/moby-programs/raise.rkt
@@ -8,4 +8,27 @@
    42)
  "hello world")
 
+
+(check-expect (exn? (with-handlers ([void identity])
+                      (raise (make-exn "foo" (current-continuation-marks)))))
+              true)
+
+(check-expect (exn:fail:contract:arity? (with-handlers ([void identity])
+                      (+ "hello" "world")))
+              false)
+
+(check-expect (exn:fail:contract? (with-handlers ([void identity])
+                      (+ "hello" "world")))
+              true)
+
+(check-expect (exn:fail:contract:arity? (with-handlers ([void identity])
+                      (identity "hello" "world")))
+              true)
+
+(check-expect (exn:fail:contract:variable? (with-handlers ([void identity])
+                      (identity "hello" "world")))
+              false)
+
+
+
 "raise.rkt end"
