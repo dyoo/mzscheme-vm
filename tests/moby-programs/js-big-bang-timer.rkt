3c73e08483f091f0ed1ea4da0591dd2cc374a2bb
Danny Yoo <dyoo@cs.wpi.edu>
2010-11-17 17:51:08 -0500

    identifying an issue with js-big-bang-timer in the running of the whole test suite.  Somthing's happening.

--- tests/moby-programs/js-big-bang-timer.rkt
+++ tests/moby-programs/js-big-bang-timer.rkt
@@ -7,9 +7,8 @@
 (printf "number should be counting up to ten\n")
 (check-expect (big-bang 1 
 			   (on-tick (lambda (w)
-				      #;(printf "~s~n" w)
+				      (printf "~s~n" w)
 				      (add1 w))
 				    1/4)
 			   (stop-when (lambda (w) (= w 10))))
 	      10)
-"All done with js-big-bang-timer!"
