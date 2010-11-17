97d5d2dd0e2ff6c16f3fab0b8d148e6393f1526f
Danny Yoo <dyoo@cs.wpi.edu>
2010-11-17 14:58:59 -0500

    getting errors to produce things that can be clicked

--- lang/check-expect/racket-display-location.rkt
+++ lang/check-expect/racket-display-location.rkt
@@ -1,6 +1,21 @@
 #lang racket/base
+(require "../location.rkt")
+
+
 (define (display-location thing a-loc)
-  (display a-loc)
-  (newline))
+  (let* ([an-stx
+          (datum->syntax #f thing (list (location-id a-loc)
+                                        (location-line a-loc)
+                                        (location-column a-loc)
+                                        (location-offset a-loc)
+                                        (location-span a-loc)))]
+         [an-error
+          (make-exn:fail:syntax ""
+                                (current-continuation-marks)
+                                (list an-stx))])
+    ((error-display-handler) (format "At: line ~a" (location-line a-loc)) 
+                             an-error)))
+    
+
 
 (provide display-location)
