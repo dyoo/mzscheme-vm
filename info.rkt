0433f48a4b3c21930962fe243e5c32743374ee9d
Danny Yoo <dyoo@cs.wpi.edu>
2010-11-19 00:59:11 -0500

    bumping up version number

--- info.rkt
+++ info.rkt
@@ -11,28 +11,10 @@
 (define categories '(devtools))
 (define can-be-loaded-with 'all)
 (define required-core-version "5.0.1")
-(define version "1.10")
+(define version "1.11")
 (define repositories '("4.x"))
 (define blurb 
   '("Provides support for compiling Racket programs to run as Javascript on the browser."))
 
 (define release-notes
-  '(
-    (p "Fixes:"
-       (ul
-
-	(li "check-expect: check-expect now works by
-    applying tests after all of the definitions and other expressions
-    have evaluated.")
-	
-	(li "jsworld reentrancy: there was an issue where jsworld behaved badly
-    (reverting to old worlds) if events came in at a rate faster than
-    it could handle them.")
-	
-	(li "member: the definition of member can been corrected to return a
-    boolean in the WeScheme (Moby) languages.")
-	
-	(li "miscellaneous fixes: corrected errors involving
-    struct-mutator-procedure, exn structures constructors, and some
-    arithmetic edge cases.")
-	(li "exposed the struct-out form for provides.")))))
+  '())
