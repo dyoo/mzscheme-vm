8796a3b456752f3cbe08faf034c0bffa73201c6d
Danny Yoo <dyoo@cs.wpi.edu>
2010-12-03 01:23:27 -0500

    still tracing issue

--- tests/moby-programs/nested-big-bang.rkt
+++ tests/moby-programs/nested-big-bang.rkt
@@ -4,12 +4,13 @@
 ;; This program does deep big bangs; we expect to see
 ;; ten levels of tocks.
 
+(define MAX-LEVEL 10)
+
 (define (tock w)
-  (printf "tock!\n")
   (js-big-bang (+ w 1)
                (on-tick tock 0.1)
-  	       (stop-when (lambda (w2) (>= w2 10)))))
+  	       (stop-when (lambda (w2) (>= w2 MAX-LEVEL)))))
 
 
 (check-expect (tock 0)
-	      10)
+	      MAX-LEVEL)
