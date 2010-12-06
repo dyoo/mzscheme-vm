bf03dde6fd324fb23d844c85ac23dcbd26e6a1d2
Danny Yoo <dyoo@cs.wpi.edu>
2010-12-06 17:03:53 -0500

    fixing typo about iscomplex

--- private/lib/types.js
+++ private/lib/types.js
@@ -1426,7 +1426,7 @@ var numberToDomNode = function(n) {
 	    return node;
 	} else if (jsnums.isRational(n)) {
 	    return rationalToDomNode(n);
-	} else if (jsnums.isComplex(n)) {
+	} else if (isComplex(n)) {
 	    node = document.createElement("span");
 	    node.appendChild(document.createTextNode(n.toString()));
 	    return node;
